ae6617e6e76073aedab971d7ba0bac565b4b5357
askcompany-kr <me@askcompany.kr>
2019-07-24 11:52:07 +0900

    pokeform_new 구현

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from pokemon.forms import PokemonForm
 from pokemon.models import Pokemon
@@ -21,10 +21,21 @@ def index(request):
 
 
 def pokemon_new(request):
-    form = PokemonForm()
-    tag = form.as_table()
-    tag = '<table>' + tag + '</table>'
-    return HttpResponse(tag)
+    if request.method == 'GET':
+        form = PokemonForm()
+    else:
+        # 뷰에서 참조할 수 있는 데이터 목록
+        # request.GET, request.POST, request.FILES
+        form = PokemonForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()  # ModelForm에서만 지원
+            return redirect('/pokemon/')
+        else:
+            form.errors
+
+    return render(request, 'pokemon/pokemon_form.html', {
+        'form': form,
+    })
 
 
 def pokemon_edit(request, pk):
